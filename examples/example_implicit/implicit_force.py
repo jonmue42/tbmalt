c05ff1957ac84546204b28e06d3c9bd94b7f6561
Jonas Müller <jonmue@uni-bremen.de>
2025-02-25 15:03:49 +0100

    did some stuff

--- examples/example_implicit/implicit_force.py
+++ examples/example_implicit/implicit_force.py
@@ -104,7 +104,7 @@ H2 = Geometry(torch.tensor([1, 1]),
 #H2O = C2H2Au2S3
 #H20 = CO2
 
-geo = H2O
+geo = H2O + H2
 
 
 orbital_info = OrbitalInfo(geo.atomic_numbers, shell_dict, shell_resolved=False)
@@ -141,6 +141,10 @@ print('Total energy:', total_energy)
 repulsive_energy = dftb_calculator.repulsive_energy
 print('Repulsive energy:', repulsive_energy)
 #print('Repulsive energy not imp:', dftb_calculator_notimp.repulsive_energy)
+overlap = dftb_calculator.overlap
+print('Overlap:', overlap)
+s_grad = torch.autograd.grad(overlap, geo.positions, grad_outputs=torch.ones_like(overlap), retain_graph=True)[0]
+print('S grad:', s_grad)
 
 # Calculate the gradient
 start_time = time.time()
