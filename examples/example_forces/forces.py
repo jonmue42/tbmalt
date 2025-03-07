095f6228c57ef89e67ff36ef29471bf59ca31ff1
Jonas Müller <jonmue@uni-bremen.de>
2025-03-07 16:00:59 +0100

    still need to make it work for batches

--- examples/example_forces/forces.py
+++ examples/example_forces/forces.py
@@ -104,9 +104,9 @@ H2 = Geometry(torch.tensor([1, 1]),
 #H20 = CO2
 
 #geo = H2
-#geo = C2H2Au2S3
+geo = C2H2Au2S3
 #geo = H2O
-geo = CO2
+#geo = H2O + H2
 
 
 orbital_info = OrbitalInfo(geo.atomic_numbers, shell_dict, shell_resolved=False)
