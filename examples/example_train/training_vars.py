c8b6b9f7e9e64548cfb2593709938cf331968aec
Jonas Müller <jonmue@uni-bremen.de>
2025-04-21 18:17:49 +0200

    did some test run and saved resulting figures

--- examples/example_train/training_vars.py
+++ examples/example_train/training_vars.py
@@ -2,13 +2,13 @@ import torch
 
 training_globals = {
         'learning_rate': 0.00005,
-        'number_of_epochs': 1,
+        'number_of_epochs': 50,
         'dos_sigma': 0.09,
         }
 
 si63v_hse_101_vars = {
-        'n_batch': 1,
-        'training_split': [2, 99],
+        'n_batch': 5,
+        'training_split': [10, 91],
         'points': torch.linspace(-3.0, 2.0, 501),
         'dataset_path': './data_wenbo/dataset/fhi-aims_si63v_hse_101.hdf',
         'parameter_db_path': './data _tbmaltpaper/siband.hdf5',
